da7c29e4065009f403201c3c925a1b9945131ca0
ecshenryho <ecshenryho@gmail.com>
2018-12-03 11:12:26 -0800

    adding for flights

--- travelex_v2/app.py
+++ travelex_v2/app.py
@@ -3,28 +3,27 @@ app=Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return "Flask App!!"
-
-
-@app.route("/hotels/")
-def hotels():
     return render_template(
     'index.html')
 
 
-# @app.route("/hotels/<string:name>/")
-# def details(name):
-#     return render_template(
-#     'page3.html',name="page3.html")
-
-@app.route("/hotels/details", methods=['POST'])
-def details():
+@app.route("/hotels", methods=['POST'])
+def hotels():
 
     if request.form.get('location'):
       loc=request.form['location']
       print(loc)
       return render_template("hoteldetails.html",loc=loc)
     return render_template("hoteldetails.html")
+
+@app.route("/flights", methods=['POST'])
+def flights():
+
+    if request.form.get('location'):
+      loc=request.form['location']
+      print(loc)
+      return render_template("flightsdetails.html",loc=loc)
+    return render_template("flightsdetails.html")
 
 
 if __name__=="__main__":
