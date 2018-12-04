818135079c8a29a918e9479d8c6fd5fd0dc36b2c
ecshenryho <ecshenryho@gmail.com>
2018-12-04 00:10:57 -0800

    add code for flight

--- travelex_v2/app.py
+++ travelex_v2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from datetime import date
 app=Flask(__name__, static_url_path='')
 
 @app.route("/")
@@ -19,11 +20,18 @@ def hotels():
 @app.route("/flights", methods=['POST'])
 def flights():
 
-    if request.form.get('location'):
-      loc=request.form['location']
-      print(loc)
-      return render_template("flightsdetails.html",loc=loc)
-    return render_template("flightsdetails.html")
+    if request.form.get('origin') and request.form.get('destination') and request.form.get('depart') and request.form.get('return'):
+      origin=request.form['origin']
+      destination=request.form['destination']
+      departdate=request.form['depart']
+      returndate=request.form['return']
+      print(origin)
+      print(destination)
+      print(departdate)
+      print(returndate)
+    #return 'origin: '+ ori + 'destination: ' + dest + 'depart: '+ dp + 'return' + rt
+      return render_template('flightsdetails.html', origin=origin, destination=destination,departdate=departdate,returndate=returndate)
+    return render_template('flightsdetails.html')
 
 
 if __name__=="__main__":
